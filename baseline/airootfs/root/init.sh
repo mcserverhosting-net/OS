b8cea2840242f50813561a80054f6e78d009a473
Samuel Walker <sfxworks@gmail.com>
2022-10-17 22:57:15 -0400

    baseline kubeadm

---
+++ baseline/airootfs/root/init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+echo Init success >> /tmp/log.txt
+
+
+sgdisk --zap-all /dev/nvme0n1
+#dd if=/dev/zero of=/dev/sda bs=1M count=100 oflag=direct,dsync
+blkdiscard nvme0n1
+
+echo "Making file system on root device" >> /tmp/log.txt
+mkfs.ext4 /dev/nvme0n1
+echo "Mounting" >> /tmp/log.txt 
+mount /dev/nvme0n1 /mnt
+echo "Enabling crio, configured to use /mnt for container runtimes" >> /tmp/log.txt
+systemctl enable --now crio 
+echo "Starting up the join..." >> /tmp/log.txt
+systemctl enable kubelet
+kubeadm join --config /etc/kubeadm/kubeadm.conf.yaml
+#kubeadm join nearest.ton618.one:6443 --token v1w7o8.w3rjbi1ezfderevp --discovery-token-ca-cert-hash sha256:d4c49500157420962b8a1287b9e23957c0e89b3095c7c50563ed1f21b5a61424
+#kubeadm -v5 join nearest.ton618.one:6443 --token wfkspa.l8g8ngzzzrvasjkt --discovery-token-ca-cert-hash sha256:d4c49500157420962b8a1287b9e23957c0e89b3095c7c50563ed1f21b5a61424
+echo "Join operation complete." >> /tmp/log.txt
---
+++ baseline/profiledef.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="archlinux-baseline"
+iso_label="ARCH_$(date +%Y%m)"
+iso_publisher="Arch Linux <https://archlinux.org>"
+iso_application="Arch Linux baseline"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlz4hc,12' -E ztailpacking)
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root/init.sh"]="0:0:755"
+)
