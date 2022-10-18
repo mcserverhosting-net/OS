06d9e1ffbc317f90b2f2a72d4f0ce99a5df4e21f
Samuel Walker <sfxworks@gmail.com>
2022-10-18 03:12:57 -0400

    ntp, kubeadm config remove name, init adjustments, services

--- baseline/airootfs/root/init.sh
+++ baseline/airootfs/root/init.sh
@@ -14,9 +14,7 @@ mount /dev/nvme0n1 /mnt
 echo "Enabling crio, configured to use /mnt for container runtimes" >> /tmp/log.txt
 systemctl enable --now crio 
 echo "Starting up the join..." >> /tmp/log.txt
-systemctl enable kubelet
-echo "arch-$RANDOM" > /etc/hostname
+UUID=$(cat /sys/class/dmi/id/product_uuid)
+echo "arch-$UUID" > /etc/hostname
 kubeadm join --config /etc/kubeadm/kubeadm.conf.yaml
-#kubeadm join nearest.ton618.one:6443 --token v1w7o8.w3rjbi1ezfderevp --discovery-token-ca-cert-hash sha256:d4c49500157420962b8a1287b9e23957c0e89b3095c7c50563ed1f21b5a61424
-#kubeadm -v5 join nearest.ton618.one:6443 --token wfkspa.l8g8ngzzzrvasjkt --discovery-token-ca-cert-hash sha256:d4c49500157420962b8a1287b9e23957c0e89b3095c7c50563ed1f21b5a61424
 echo "Join operation complete." >> /tmp/log.txt
