137a86e08338089b4bebdb886775fe38960b5c1e
Samuel Walker <sfxworks@gmail.com>
2022-10-18 03:28:16 -0400

    unique id

--- baseline/airootfs/root/init.sh
+++ baseline/airootfs/root/init.sh
@@ -6,15 +6,14 @@ echo Init success >> /tmp/log.txt
 sgdisk --zap-all /dev/nvme0n1
 #dd if=/dev/zero of=/dev/sda bs=1M count=100 oflag=direct,dsync
 blkdiscard /dev/nvme0n1
-
-echo "Making file system on root device" >> /tmp/log.txt
 mkfs.ext4 /dev/nvme0n1
-echo "Mounting" >> /tmp/log.txt 
 mount /dev/nvme0n1 /mnt
-echo "Enabling crio, configured to use /mnt for container runtimes" >> /tmp/log.txt
+
 systemctl enable --now crio 
-echo "Starting up the join..." >> /tmp/log.txt
-UUID=$(cat /sys/class/dmi/id/product_uuid)
-echo "arch-$UUID" > /etc/hostname
+UUID=$(cat /sys/class/dmi/id/board_serial)
+envsubst < /etc/kubeadm/kubeadm.conf.yaml | sponge /etc/kubeadm/kubeadm.conf.yaml
+echo $UUID > /etc/hostname
+echo "127.0.0.1 $UUID" > /etc/hosts
+echo "::1 $UUID" >> /etc/hosts
 kubeadm join --config /etc/kubeadm/kubeadm.conf.yaml
 echo "Join operation complete." >> /tmp/log.txt
