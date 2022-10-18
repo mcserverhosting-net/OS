f1f0bf2d28c3449d17ddbe55b942c9989f8e3339
Samuel Walker <sfxworks@gmail.com>
2022-10-18 02:57:47 -0400

    hostname and join config version

--- baseline/airootfs/root/init.sh
+++ baseline/airootfs/root/init.sh
@@ -15,6 +15,7 @@ echo "Enabling crio, configured to use /mnt for container runtimes" >> /tmp/log.
 systemctl enable --now crio 
 echo "Starting up the join..." >> /tmp/log.txt
 systemctl enable kubelet
+echo "arch-$RANDOM" > /etc/hostname
 kubeadm join --config /etc/kubeadm/kubeadm.conf.yaml
 #kubeadm join nearest.ton618.one:6443 --token v1w7o8.w3rjbi1ezfderevp --discovery-token-ca-cert-hash sha256:d4c49500157420962b8a1287b9e23957c0e89b3095c7c50563ed1f21b5a61424
 #kubeadm -v5 join nearest.ton618.one:6443 --token wfkspa.l8g8ngzzzrvasjkt --discovery-token-ca-cert-hash sha256:d4c49500157420962b8a1287b9e23957c0e89b3095c7c50563ed1f21b5a61424
