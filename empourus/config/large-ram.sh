652cb605f0df139dd1daf11ea64fab5d318a0f26
Samuel Walker <sfxworks@gmail.com>
2022-12-05 06:50:51 -0500

    read only fs so must use cp instead of mv

--- empourus/config/large-ram.sh
+++ empourus/config/large-ram.sh
@@ -11,7 +11,7 @@ systemctl enable --now crio
 
 
 export UUID=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address | sed s/://g )
-mv /config/kubeadm/*.yaml /etc/kubeadm/kubeadm.conf.yaml
+cp /config/kubeadm/*.yaml /etc/kubeadm/kubeadm.conf.yaml
 envsubst < /etc/kubeadm/kubeadm.conf.yaml | sponge /etc/kubeadm/kubeadm.conf.yaml
 echo "home-$UUID" > /etc/hostname
 echo "127.0.0.1 home-$UUID" > /etc/hosts
