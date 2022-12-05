bac542dd19f860584b688722b63f0e063c7a7f8f
Samuel Walker <sfxworks@gmail.com>
2022-12-05 05:19:38 -0500

    script update

--- make.sh
+++ make.sh
@@ -4,16 +4,15 @@ set -e
 #Make the ISO
 sudo podman run --privileged --network=host -v ./baseline:/profile registry.service.mcserverhosting.net/library/archiso:latest mkarchiso -v -w /tmp -o /profile/out /profile -quiet=y
 #Mount the ISO
-sudo mkdir -p /mnt/iso
 #Only the first will me mounted
-sudo mount -o loop $PWD/baseline/out/* /mnt/iso
+sudo mount -o loop $PWD/baseline/out/* $PWD/empourus/boot
 
 #Build the collection
-empourus --loglevel=debug build collection . registry.service.mcserverhosting.net/dev/os:latest --dsconfig empourus/dataset.yaml
+empourus --loglevel=debug build collection empourus registry.service.mcserverhosting.net/dev/os:latest --dsconfig empourus/dataset.yaml
 
 # Unmount the ISO
-sudo umount /mnt/iso
+sudo umount $PWD/empourus/boot
 
 #Push the collection
-empourus --loglevel=debug push registry.service.mcserverhosting.net/dev/os:latest
+empourus --loglevel=debug push registry.service.mcserverhosting.net/dev/os:latest --insecure --plain-http
 
