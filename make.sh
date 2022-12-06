7ed52b3e6a5feeda6095959a73b9b272cecf2977
Samuel Walker <sfxworks@gmail.com>
2022-12-06 10:24:23 -0500

    new make

--- make.sh
+++ make.sh
@@ -2,7 +2,7 @@
 set -e
 
 #Make the ISO
-sudo podman run --privileged --network=host -v ./baseline:/profile registry.service.mcserverhosting.net/library/archiso:latest mkarchiso -v -w /tmp -o /profile/out /profile -quiet=y
+sudo podman run --privileged -v ./baseline:/profile registry.service.mcserverhosting.net/library/archiso:latest mkarchiso -v -w /tmp -o /profile/out /profile -quiet=y
 #Mount the ISO
 #Only the first will me mounted
 sudo mount -o loop $PWD/baseline/out/* $PWD/empourus/boot
