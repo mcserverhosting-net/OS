15c73a4e647379a1f5cc05b72fb2c6394314c7c5
Samuel Walker <sfxworks@gmail.com>
2023-04-11 01:28:58 -0400

    Select first non-empty disk or wipe empty disk for mount
    - Updated the script to iterate through a list of candidate disks (nvme0n1, sda, vda).
    - The script now selects the first available non-empty disk to use as a mount.
    - If no non-empty disks are found, the script wipes and formats the first empty disk.
    - If no suitable disk is found, the script falls back to using a ramdisk.
    - Added comments and functions for better readability and organization.

--- init.sh
+++ init.sh
@@ -1,31 +1,70 @@
 #!/bin/sh
-if ls -lah /dev/nvme0n1; then
-  echo "Using an nvme." >> /tmp/log.txt
-  export DISK=/dev/nvme0n1 
-  sgdisk --zap-all $DISK
-  blkdiscard $DISK
-elif ls -lah /dev/sda; then
-  echo "Using the first sata device." >> /tmp/log.txt
-  export DISK=/dev/sda
-  sgdisk --zap-all $DISK
-  dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
-elif ls -lah /dev/vda; then
-  echo "Using the first vda device." >> /tmp/log.txt
-  export DISK=/dev/vda
-  sgdisk --zap-all $DISK
-  dd if=/dev/zero of="$DISK" bs=1M count=100 oflag=direct,dsync
-else
-  echo "No disk device usable. Using a ramdisk. Assuming at least 16G of memory availible."
-  #TODO: Use `grep MemTotal /proc/meminfo` and do either 16G or half of memory availbile, whichever is less. 
+
+# Define a list of candidate disks to check
+candidate_disks="/dev/nvme0n1 /dev/sda /dev/vda"
+
+# Function to check if a disk is empty (no partitions)
+is_disk_empty() {
+  local disk="$1"
+  local partition_count
+
+  # Get the number of partitions on the disk
+  partition_count=$(parted --script "$disk" print | grep -c '^Number')
+
+  # Check if there are no partitions
+  [ "$partition_count" -eq 0 ]
+}
+
+# Function to wipe and format a disk
+wipe_and_format_disk() {
+  local disk="$1"
+
+  sgdisk --zap-all "$disk"
+  if [ "$(basename "$disk")" = "nvme0n1" ]; then
+    blkdiscard "$disk"
+  else
+    dd if=/dev/zero of="$disk" bs=1M count=100 oflag=direct,dsync
+  fi
+
+  mkfs.ext4 "$disk"
+}
+
+# Iterate through the candidate disks
+for disk in $candidate_disks; do
+  # Check if the disk exists
+  if ls -lah "$disk" >/dev/null 2>&1; then
+    # Check if the disk is empty
+    if is_disk_empty "$disk"; then
+      # Wipe and format the empty disk
+      echo "Disk $disk is empty. Wiping and formatting it." >> /tmp/log.txt
+      wipe_and_format_disk "$disk"
+      export DISK="$disk"
+      break
+    else
+      # Use the non-empty disk as a mount
+      echo "Using non-empty disk $disk as a mount." >> /tmp/log.txt
+      export DISK="$disk"
+      break
+    fi
+  fi
+done
+
+# If no suitable disk was found, use a ramdisk
+if [ -z "$DISK" ]; then
+  echo "No disk device usable. Using a ramdisk. Assuming at least 16G of memory available." >> /tmp/log.txt
+  #TODO: Use `grep MemTotal /proc/meminfo` and do either 16G or half of memory available, whichever is less.
   modprobe zram num_devices=1
   echo 16G > /sys/block/zram0/disksize
   export DISK=/dev/zram0
+  mkfs.ext4 "$DISK"
 fi
 
-mkfs.ext4 $DISK
-mount $DISK /mnt
-mount $DISK /var/log
+# Mount the disk to /mnt and /var/log
+mount "$DISK" /mnt
+mount "$DISK" /var/log/pods
+mount "$DISK" /var/log/containers
 mkdir /mnt/tmp
+
 
 export UUID=$(cat /sys/class/net/$(ip route show default | awk '/default/ {print $5}')/address | sed s/://g )
 
