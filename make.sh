ddeb2c8b84c15ac0a39c309cbe4ec676e6d060f3
Samuel Walker <services@sfxworks.net>
2024-10-19 01:10:26 -0400

    mv adjustments

--- make.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-
-#Make the ISO
-sudo docker run --privileged -v ./baseline:/profile -v $PWD/customrepo:/customrepo -v /mnt/nfs:/profile/out harbor.home.sfxworks.net/library/archiso mkarchiso -v -w /tmp -o /profile/out /profile -quiet=y
